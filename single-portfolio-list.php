7c13cec38c7116159a0e20de25133185dff7aaf2
tiganiacristian <cristi.tigania@vuzum.com>
2015-08-28 17:07:21 +0300

    added updates to single page

--- single-portfolio-list.php
+++ single-portfolio-list.php
@@ -1,17 +1,17 @@
-	<?php get_header(); ?>
+<?php get_header(); ?>
 
 	<div class="portofolio" id="portofolio-menu">
 	    <div class="inner-content">
-	     <?php if ( have_posts() ) : while (have_posts() ) : the_post(); ?>
-	    <div>
-	     <?php the_post_thumbnail('slider'); ?>
+	   		<?php if ( have_posts() ) : while (have_posts() ) : the_post(); ?>
+	   			<div class="project-center">
+	    			 <?php the_post_thumbnail('slider'); ?>
+	    		</div>
+	    		<div>
+	     			<h2 class="portofolio_title portofolio_title--single"><?php the_title(); ?></h2>
+	      			<p class="portofolio_text"><?php echo get_the_content(); ?></p>     
+	   			</div>
+	  		<?php endwhile; endif; ?>
 	    </div>
-	    <div>
-	     <h2 class="portofolio_title"><?php the_title(); ?></h2>
-	      <p class="portofolio_text"><?php echo get_the_content(); ?></p>     
-	    </div>
-	   <?php endwhile; endif; ?>
-	    </div>
-	 </div>
+	</div>
 
-	<?php get_footer(); ?>
+<?php get_footer(); ?>
